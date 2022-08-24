e5420b9a322a8bfe6290acf72b170c90cf858fb2
lndsilva <lndsilva@hotmail.com>
2022-08-23 22:10:17 -0300

    navviewapp menu toolbar view toogle

--- app/src/main/java/br/com/etecia/myappnavview/MainActivity.java
+++ app/src/main/java/br/com/etecia/myappnavview/MainActivity.java
@@ -1,6 +1,8 @@
 package br.com.etecia.myappnavview;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
 import android.view.View;
@@ -9,6 +11,7 @@ import com.google.android.material.appbar.MaterialToolbar;
 
 public class MainActivity extends AppCompatActivity {
     MaterialToolbar toolbar;
+    DrawerLayout drawerLayout;
 
 
     @Override
@@ -18,12 +21,19 @@ public class MainActivity extends AppCompatActivity {
 
         toolbar = findViewById(R.id.toolBar);
 
+        drawerLayout = findViewById(R.id.idNavDrawer);
+
         toolbar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
             }
         });
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,
+                drawerLayout, toolbar, R.string.navopen, R.string.navclose);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
 
 
     }
