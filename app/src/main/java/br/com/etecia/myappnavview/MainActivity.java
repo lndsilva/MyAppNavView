61bcecb92650014f3ce61da6a5e252980fa95198
lndsilva <lndsilva@hotmail.com>
2022-08-23 21:55:32 -0300

    navviewapp menu toolbar view

--- app/src/main/java/br/com/etecia/myappnavview/MainActivity.java
+++ app/src/main/java/br/com/etecia/myappnavview/MainActivity.java
@@ -3,12 +3,28 @@ package br.com.etecia.myappnavview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.appbar.MaterialToolbar;
 
 public class MainActivity extends AppCompatActivity {
+    MaterialToolbar toolbar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = findViewById(R.id.toolBar);
+
+        toolbar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+
     }
 }
